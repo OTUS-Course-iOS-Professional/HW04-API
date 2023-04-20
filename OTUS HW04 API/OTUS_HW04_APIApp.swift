5bec57d90e7caae1bf7227c7827c6116ccb1fce7
nerzul88 <nerzul.88@gmail.com>
2023-04-20 21:35:45 +0300

    Initial commit

---
+++ OTUS HW04 API/OTUS_HW04_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_HW04_APIApp.swift
+//  OTUS HW04 API
+//
+//  Created by Александр Касьянов on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_HW04_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
